60e78b1ab1ce96ade86b651fef4989a82d4c4280
achrafBenHamou <achraf.ben130@gmail.com>
2020-12-04 20:09:58 +0100

    initial front & back end

---
+++ Back_end/webSem/webSem/src/main/java/com/websem/webSem/WebSemApplication.java
@@ -0,0 +1,13 @@
+package com.websem.webSem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSemApplication.class, args);
+	}
+
+}
---
+++ Back_end/webSem/webSem/src/test/java/com/websem/webSem/WebSemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websem.webSem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
