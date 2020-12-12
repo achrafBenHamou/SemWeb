4b1a8d4af44ef058ec0bf868da9440ba6404d83a
achrafBenHamou <achraf.ben130@gmail.com>
2020-12-12 12:09:51 +0100

    new

---
+++ Back_end/semweb/src/test/java/com/semweb/SemwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
